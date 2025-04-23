1059a74d158e934b9a6b3ffe7d251fe39fd8ff14
Júlia Penz <juliapenz_@hotmail.com>
2025-04-23 22:45:39 +0100

    chore(hero): adding hero

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css'
 import About from './components/About/about'
 import Header from './components/Header/header'
+import Hero from './components/Hero/hero'
 
 function App() {
   return (
     <>
     <Header></Header>
+    <Hero></Hero>
     <About></About>
     </>
   )
---
+++ src/components/Hero/hero.tsx
@@ -0,0 +1,17 @@
+import "./hero.css";
+
+const Hero = () => {
+    return (
+        <div className='hero-container'>
+            <div className='hero-p'>
+                <p>Seja bem-vindo (a) a um lugar de escuta, compreensão e apoio para promover o seu bem-estar emocional.</p>
+            </div>
+            <div className='hero-text'>
+                <h1>Francine Rodrigues Penz</h1>
+                <h2>Psicóloga Clínica | CPR 07/40098</h2>
+            </div>
+        </div>
+    )
+}
+
+export default Hero;
